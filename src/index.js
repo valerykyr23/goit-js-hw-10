e57a51e0d79b62d286c3aef2582981edf702ad03
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2023-01-05 20:45:46 +0200

    добавляет решение задачи

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './css/styles.css';
 
 import _debounce from "lodash.debounce";
 
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({ cssAnimationStyle: "zoom", fontAwesomeIconStyle: "shadow" });
+
 const DEBOUNCE_DELAY = 300;
 
 
@@ -20,16 +23,18 @@ refs.input.addEventListener("input", _debounce(onSearch, DEBOUNCE_DELAY ));
 function onSearch(event) {
     
     const inputValue = event.target.value;
+    console.log(inputValue);
+   
+
     fetchCountries(inputValue)
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
+        .then(data => onSuccess(data))
+        .catch(error => callError(error));
 }
 
 function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => {
-
-        console.log(response);
-
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(response => {
+            
         if (!response.ok) {
      throw new Error(response.statusText)
         }
@@ -39,5 +44,26 @@ function fetchCountries(name) {
 }
 
 
+function callError () { 
+Notiflix.Notify.failure("Oops, there is no country with that name");
+}
 
+
+function onSuccess(obj) {
+
+    let c = obj[0];
+
+    if (obj.length > 10) {
+        Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+    }
+    
+    const markup = obj.map(obj => `<li><img src="${c.flags.svg}" alt="Country flag" width="30" >
+    <h2>${c.name.official}</h2>
+    <h3>Capital: ${c.capital}</h3>
+    <h4>Population: ${c.population}</h4>
+    <span>Languages: ${Object.values(c.languages)}</span>
+    </li>`)
+
+    refs.list.innerHTML = markup.join("");
+}
 
